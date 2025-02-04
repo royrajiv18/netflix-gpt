8cbc07deacdb229f35b5747d6676bb2ac2f81c65
Rajiv Roy <rajivpune18@gmail.com>
2025-02-04 11:55:11 +0530

    fixed sign in bugs

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,13 +7,11 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [isSignInForm, setSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const name = useRef(null);
@@ -63,15 +61,12 @@ const Login = () => {
                   photoURL: photoURL,
                 })
               );
-
-              navigate("./browse");
             })
             .catch((error) => {
               // An error occurred
               setErrorMessage(error.message);
               // ...
             });
-          console.log(user);
           // ...
         })
         .catch((error) => {
@@ -89,8 +84,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          console.log(user);
-          navigate("./browse");
           // ...
         })
         .catch((error) => {

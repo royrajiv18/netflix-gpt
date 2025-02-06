01e92739e4c9a79018c3749674ad80d43196a0fc
Rajiv Roy <rajivpune18@gmail.com>
2025-02-06 21:17:46 +0530

    gpt search button

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -6,8 +6,11 @@ import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import { useSelector } from "react-redux";
+import GptSearch from "./GptSearch";
 
 const Browse = () => {
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   useNowPlayingMovies();
   usePopularMovies();
   useUpcomingMovies();
@@ -15,8 +18,14 @@ const Browse = () => {
   return (
     <div>
       <Header />
-      <MainContainer />
-      <SecondaryContainer />
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 export const Header = () => {
   const dispatch = useDispatch();
@@ -42,11 +43,22 @@ export const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    // Toggle search gpt
+    dispatch(toggleGptSearchView());
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" alt="logo" src={LOGO} />
       {user && (
-        <div className="flex">
+        <div className="flex p-2">
+          <button
+            className="py-2 my-2 px-4 mx-4 text-white rounded-lg bg-purple-800"
+            onClick={handleGptSearchClick}
+          >
+            GPT Search
+          </button>
           <img
             alt="usericon"
             className="h-11 w-11 rounded-full"
